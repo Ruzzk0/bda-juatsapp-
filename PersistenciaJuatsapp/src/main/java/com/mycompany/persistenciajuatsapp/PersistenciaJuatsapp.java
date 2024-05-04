5ea3ec7bea950ef20c7f8e14d4f86ddccb0aef8b
Ruzzk0 <146685007+Ruzzk0@users.noreply.github.com>
2024-05-03 18:33:20 -0700

    apenas voy iniciando
    si

---
+++ PersistenciaJuatsapp/src/main/java/com/mycompany/persistenciajuatsapp/PersistenciaJuatsapp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.persistenciajuatsapp;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PersistenciaJuatsapp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
